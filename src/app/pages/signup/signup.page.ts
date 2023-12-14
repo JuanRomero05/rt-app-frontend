2bede4e96b74c4cc5adb603c4f82cadff3f45a93
Juan <jromero@sysmodelers.com>
2023-12-14 01:59:43 -0400

    Signup implemented

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  showPassword: boolean = false;
+  showRepeatPassword: boolean = false;
+
+  eyeIcon: string = 'eye-outline';
+  eyeIconRepeat: string = 'eye-outline';
+
+  selectedCheckbox: string = '';
+
+  @ViewChild('passwordInput') passwordInput: any;
+  @ViewChild('repeatPasswordInput') repeatPasswordInput: any;
+
+  constructor(
+    public fb: FormBuilder,
+    private router: Router,
+  ) {
+    this.signupForm = this.fb.group({
+      'username': new FormControl("", Validators.required),
+      'firstName': new FormControl("", Validators.required),
+      'lastName': new FormControl("", Validators.required),
+      /* 'bio': new FormControl("", Validators.required), */
+      'password': new FormControl("", Validators.required),
+      'repeatPassword': new FormControl("", Validators.required),
+    }, { validator: this.pwMatchValidator });
+  }
 
   ngOnInit() {
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+    this.passwordInput.type = this.showPassword ? 'text' : 'password';
+    this.eyeIcon = this.showPassword ? 'eye-off' : 'eye-outline';
+
+  }
+
+  toggleRepeatPasswordVisibility() {
+    this.showRepeatPassword = !this.showRepeatPassword;
+    this.repeatPasswordInput.type = this.showRepeatPassword ? 'text' : 'password';
+    this.eyeIconRepeat = this.showRepeatPassword ? 'eye-off' : 'eye-outline';
+  }
+
+  pwMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const repeatPassword = control.get('repeatPassword')?.value;
+
+    if (password !== repeatPassword) {
+      control.get('repeatPassword')?.setErrors({ 'passwordMismatch': true });
+
+    } else {
+      control.get('repeatPassword')?.setErrors(null);
+
+    }
+  }
+
+  async saveData() {
+    await this.router.navigate(['/login']);
   }
 
 }

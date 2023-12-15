34bcdcc7caca638cc75e2a4d54d139e7b1f91298
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-12-15 16:21:01 -0400

    fix: add chat functionality and add profile icon (critic/public)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io'
+import { environment as env } from 'src/environments/environment';
+
+const config: SocketIoConfig = { url: env.socketSrv, options: {} };
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SocketIoModule.forRoot(config)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
